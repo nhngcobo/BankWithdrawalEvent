ff52b152d1b6ff7402263fe07e32c6f9ec4be172
Nhlanhla <nfortunengcobo@gmail.com>
2025-04-06 20:19:58 +0200

    See CHANGES updated WithdrawalEvent

--- src/main/java/org/example/bankwithdrawalevent/WithdrawalEvent.java
+++ src/main/java/org/example/bankwithdrawalevent/WithdrawalEvent.java
@@ -4,9 +4,9 @@ package org.example.bankwithdrawalevent;
 import java.math.BigDecimal;
 
 public class WithdrawalEvent {
-    private BigDecimal amount;
-    private Long accountId;
-    private String status;
+    private final BigDecimal amount;
+    private final Long accountId;
+    private final String status;
     public WithdrawalEvent(BigDecimal amount, Long accountId, String status) {
         this.amount = amount;
         this.accountId = accountId;
